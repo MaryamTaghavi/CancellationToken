bd45e3a296dce439fbddf9aa7fd6331ac721831c
MaryamTaghavi <m.taghavi@megagroup.biz>
2024-06-22 16:28:33 +0330

    pushCode

---
+++ CancellationToken/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+    CancellationTokenSource source = new();
+    Console.WriteLine(source.Token.IsCancellationRequested);
+    await GetAllDataFromDatabaseAsync(source.Token);
+    
+    source.Cancel();
+   Console.WriteLine(source.Token.IsCancellationRequested);
+
+    await GetAllDataFromDatabaseAsync(source.Token);
+
+
+static async Task GetAllDataFromDatabaseAsync(CancellationToken cancellationToken)
+{
+        HttpClient client = new();
+        var result = await client.GetAsync("https://google.com", cancellationToken);
+        
+    Console.WriteLine(result);
+}
